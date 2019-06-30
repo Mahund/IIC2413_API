46ac7a416a9eb5b55d51fe237763e80014f9f6be
Michael Hund <mahund@uc.cl>
2019-06-30 16:50:44 -0400

    Comented mongod open db

--- main.py
+++ main.py
@@ -21,9 +21,9 @@ MESSAGES_KEYS = ["message","lat","long","date"] # deleted sender and receptant
 
 # Levantamos el servidor de mongo. Esto no es necesario, puede abrir
 # una terminal y correr mongod. El DEVNULL hace que no vemos el output
-mongod = subprocess.Popen('mongod', stdout=subprocess.DEVNULL)
+# mongod = subprocess.Popen('mongod', stdout=subprocess.DEVNULL)
 # Nos aseguramos que cuando el programa termine, mongod no quede corriendo
-atexit.register(mongod.kill)
+# atexit.register(mongod.kill)
 
 # El cliente se levanta en localhost:5432
 client = MongoClient('localhost')
